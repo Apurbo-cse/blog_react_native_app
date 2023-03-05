d58c0313743df4558319efd0d3b8b5f82ecbaa1e
Apurbo Kumar Anup <apurboka@gmail.com>
2023-03-05 09:57:25 +0600

    Update Create.js

--- src/screens/main/Create.js
+++ src/screens/main/Create.js
@@ -32,22 +32,20 @@ const Create = ({ route }) => {
   };
 
   const handelSave = () => {
+    var data = {
+      name: coureName,
+      price: Number(coursePrice) || 0,
+      description: description,
+    };
 
-      var data = {
-        name: coureName,
-        price: Number(coursePrice) || 0,
-        description: description,
-      };
-
-      axios({
-        url: "http://192.168.0.208:81/api_native/public/api/store",
-        method: "POST",
-        data: data,
-        headers: {
-          "Content-Type": "application/json",
-        },
-      })
-    
+    axios({
+      url: "http://192.168.0.208:81/api_native/public/api/store",
+      method: "POST",
+      data: data,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
   };
 
   return (
@@ -55,7 +53,6 @@ const Create = ({ route }) => {
       <CustomHeader title="Post" backFunc={() => navigation.goBack()} />
 
       <View style={styles.form}>
-        
         <TextInput
           value={coureName}
           style={styles.text_input}
@@ -79,8 +76,6 @@ const Create = ({ route }) => {
           <Text style={styles.textButton}>Save</Text>
         </TouchableOpacity>
       </View>
-
-
     </SafeAreaView>
   );
 };
@@ -88,89 +83,28 @@ const Create = ({ route }) => {
 export default Create;
 
 const styles = StyleSheet.create({
+  form: {
+    padding: 15,
+    backgroundColor: "#e3e3e3",
+  },
 
+  text_input: {
+    padding: 10,
+    borderWidth: 1,
+    borderColor: "gray",
+    borderRadius: 10,
+    marginTop: 10,
+  },
 
-    form:{
-        padding : 15,
-        backgroundColor : "#e3e3e3",
-    },
-   
-    txtClose:{
-        fontSize:18,
-        fontWeight : "bold",
-        marginVertical : 10,
-        textAlign : "right"
-    },
-    text_input:{
-        padding :10,
-        borderWidth :1,
-        borderColor : "gray",
-        borderRadius : 10,
-        marginTop :10
-    },
-    header_container : {
-        padding : 15,
-        backgroundColor : "#eeeeee",
-        flexDirection:"row",
-        justifyContent : "space-between"
-    },
-    txt_main : {
-        fontSize : 22,
-        fontWeight : "bold"
-    },
-    item_course : {
-        padding :15,
-        borderBottomWidth: 1,
-        borderBottomColor : "#e2e2e2",
-        flexDirection : "row",
-        justifyContent:"space-between"
-    },
-    txt_name : {
-        fontSize : 18,
-        marginTop : 5,
-        fontWeight : "bold"
-    },
-    txt_item : {
-        fontSize : 14,
-        marginTop : 5
-    },
-    txt_enabled : {
-        fontSize : 14,
-        marginTop : 5,
-        color:"green",
-        fontWeight : "bold"
-    },
-    txt_disabled : {
-        fontSize : 14,
-        marginTop : 5,
-        color:"green",
-        fontWeight : "bold"
-    },
-    txt_del:{
-        fontSize : 14,
-        marginTop : 5,
-        color:"red",
-        fontWeight : "bold"
-    },
-    txt_edit:{
-        fontSize : 14,
-        marginTop : 5,
-        color:"blue",
-        fontWeight : "bold"
-    },
-    btnContainer : {
-        display : 'flex',
-        padding :15,
-        backgroundColor : "#000",
-        marginTop : 20,
-        
-    },
-    btnNewContainer : {
-        padding :10,
-        backgroundColor : "#000",
-    },
-    textButton : {
-        textAlign : "center",
-        color : "#FFF"
-    },
+  btnContainer: {
+    display: "flex",
+    padding: 15,
+    backgroundColor: "#000",
+    marginTop: 20,
+  },
+
+  textButton: {
+    textAlign: "center",
+    color: "#FFF",
+  },
 });
