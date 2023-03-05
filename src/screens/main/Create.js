31359b7c257a11805a8cba0ce123e928b14767bb
Apurbo Kumar Anup <apurboka@gmail.com>
2023-03-05 15:44:18 +0600

    Update

--- src/screens/main/Create.js
+++ src/screens/main/Create.js
@@ -5,34 +5,84 @@ import {
   TextInput,
   SafeAreaView,
   TouchableOpacity,
+  Modal,
+  Button,
 } from "react-native";
 import React, { useState } from "react";
+
 import CustomHeader from "../../components/CustomHeader";
 import { useNavigation } from "@react-navigation/native";
 
 import axios from "axios";
+import RNDateTimePicker from '@react-native-community/datetimepicker';
+
 
 const Create = ({ route }) => {
   const navigation = useNavigation();
 
+  const [date, setDate] = useState(new Date(1598051730000));
+  // const [date, setDate] = useState(new Date())
+  const [mode, setMode] = useState('date')
+  const [show, setShow] = useState(false)
+  const [text, setText] = useState('Epmty')
+
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate;
+    // setShow()
+
+    setShow(false);
+    setDate(currentDate);
+
+    // setDate(currentDate);
+
+    // let temDate = new Date(currentDate)
+    // let fDate = temDate.getDate() + '-' + (temDate.getMonth() + 1) + '-' + temDate.getFullYear()
+    // let fTime = temDate.getHours() + ':' + temDate.getMinutes()
+    // setText(fDate + ' ' + fTime)
+    // console.log('==============> ', fDate + ' ' + fTime)
+
+  }
+
+  // const showMode = (currentMode) => {
+  //   setShow(true)
+  //   setMode(currentMode)
+  // }
+
+  const showMode = (currentMode) => {
+    // if (Platform.OS === 'android') {
+    //   setShow(false);
+     
+    // }
+    setMode(currentMode);
+  };
+
+  const showDatepicker = () => {
+    console.log('Clicked');
+    showMode('date');
+  };
+
+  const showTimepicker = () => {
+    showMode('time');
+  };
+
+  
   const [coureName, setCourseName] = useState("");
   const [coursePrice, setCoursePrice] = useState(0);
   const [description, setDescriptoin] = useState("");
 
+
+  
   const onChangeName = (value) => {
     setCourseName(value);
   };
 
-  
   const onChangePrice = (value) => {
     setCoursePrice(value);
   };
 
-
   const onChangeDescriptoin = (value) => {
     setDescriptoin(value);
   };
-
 
   const handelSave = () => {
     var data = {
@@ -55,7 +105,20 @@ const Create = ({ route }) => {
     <SafeAreaView style={{ flex: 1, backgroundColor: "#000" }}>
       <CustomHeader title="Post" backFunc={() => navigation.goBack()} />
 
+
+
       <View style={styles.form}>
+
+        <Button title="Date" onPress={showDatepicker} />
+
+        {show && (<DateTimePickerAndroid
+        testId="dateTimePicker"
+        value={date}
+        mode={mode}
+        is24Hour={true}
+        // display="default"
+        onChange={onChange}
+        />)}
         <TextInput
           value={coureName}
           style={styles.text_input}
@@ -74,6 +137,10 @@ const Create = ({ route }) => {
           placeholder="Description"
           onChangeText={onChangeDescriptoin}
         />
+
+
+   
+
 
         <TouchableOpacity onPress={handelSave} style={styles.btnContainer}>
           <Text style={styles.textButton}>Save</Text>
@@ -109,5 +176,25 @@ const styles = StyleSheet.create({
   textButton: {
     textAlign: "center",
     color: "#FFF",
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignContent: "center",
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    width: "90%",
+    alignContent: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    elevation: 5,
   },
 });
